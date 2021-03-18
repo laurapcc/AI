df5e0ce00c527cf73c572745718d4646b9cea960
jorge.miguelp <jorge.miguelp@estudiante.uam.es>
2021-03-18 21:28:38 +0100

    Carpeta con archivos para prueba timeit

---
+++ Practice2/code/Prueba Timeit/2351_p2_07_demiguel_depaz.py
@@ -0,0 +1,197 @@
+###############################################
+#                                             #
+#        Heuristics for tournament            #
+#                                             #
+# Authors: Jorge de Miguel Pires              #
+#          <jorge.miguelp@estudiante.uam.es>  #
+#                                             #
+#          Laura de Paz Carbajo               #
+#          <laura.pazc@estudiante.uam.es>     #
+# Team 07                                     #
+# Group 2351                                  #
+#                                             #
+###############################################
+
+import time
+from game import TwoPlayerGameState
+
+from tournament import StudentHeuristic
+from reversi import Reversi
+
+
+"""
+Heuristic class whose evaluation function calculates the difference of each
+player pieces with regard to the total amount of pieces placed in the board
+"""
+
+
+class PieceDifference(StudentHeuristic):
+
+    def get_name(self) -> str:
+        return "P07_Heuristic1"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        value = evalPieces(state)
+        if state.is_player_max(state.player1):
+            return value
+        elif state.is_player_max(state.player2):
+            return -value
+        raise ValueError('Player MAX not defined')
+
+
+"""
+Heuristic class whose evaluation function calculates the amount of pieces each
+player has on the edges of the board and computes its difference
+"""
+
+
+class Edges(StudentHeuristic):
+
+    def get_name(self) -> str:
+        return "P07_Heuristic2"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        value = evalEdges(state)
+        if state.is_player_max(state.player1):
+            return value
+        elif state.is_player_max(state.player2):
+            return -value
+        raise ValueError('Player MAX not defined')
+
+
+"""
+Heuristic class whose evaluation function calculates the amount of pieces each
+player has on the corners of the board and computes its difference
+A player should aim to capture these positions, as they can never be replaced
+with the opponent's pieces.
+"""
+
+
+class Corners(StudentHeuristic):
+
+    def get_name(self) -> str:
+        return "P07_Heuristic3"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        value = evalCorners(state)
+        if state.is_player_max(state.player1):
+            return value
+        elif state.is_player_max(state.player2):
+            return -value
+        raise ValueError('Player MAX not defined')
+
+
+"""
+Heuristic class whose evaluation function calculates the amount of pieces each
+player has on the edges of the board and computes its difference
+"""
+
+
+class EdgesAndCorners(StudentHeuristic):
+
+    def get_name(self) -> str:
+        return "P07_Heuristic4"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        edges = evalEdges(state)
+        corners = evalCorners(state)
+        value = edges + corners
+
+        if state.is_player_max(state.player1):
+            return value
+        elif state.is_player_max(state.player2):
+            return -value
+        raise ValueError('Player MAX not defined')
+
+        return Edges().evaluation_function(state) + \
+            Corners().evaluation_function(state)
+
+
+"""
+Heuristic class whose evaluation function calculates the amount of pieces,
+pieces in the edge of the bord and pieces in corners that both players
+have and computes its difference
+"""
+
+
+class PiecesEdgesCorners(StudentHeuristic):
+    def get_name(self) -> str:
+        return "PiecesEdgesCorners"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        pieces = evalPieces(state)
+        edges = evalEdges(state)
+        corners = evalCorners(state)
+        value = pieces + edges + corners
+
+        if state.is_player_max(state.player1):
+            return value
+        elif state.is_player_max(state.player2):
+            return -value
+        raise ValueError('Player MAX not defined')
+
+
+# Private functions
+"""
+evalPieces(): calculates the percentage of the pieces in
+the board that belong to the player that has the turn
+"""
+
+
+def evalPieces(state):
+    scores = state.scores
+    return 100 * (scores[0] - scores[1]) / (scores[0] + scores[1])
+
+
+"""
+evalEdges(): calculates the percentage of the pieces in the
+edges of the board that belong to the player that has the turn
+"""
+
+
+def evalEdges(state):
+    game = state.game
+    board = state.board
+    assert isinstance(game, Reversi)  # only Reversi has height and width
+    height = game.height
+    width = game.width
+
+    edgeLeft = [board.get((x, 1)) for x in range(1, height+1)]
+    edgeRight = [board.get((x, width)) for x in range(1, height+1)]
+    edgeTop = [board.get((1, x)) for x in range(1, width+1)]
+    edgeBottom = [board.get((height, x)) for x in range(1, width+1)]
+
+    edges1 = edgeLeft.count(game.player1.label) + \
+        edgeRight.count(game.player1.label) + \
+        edgeTop.count(game.player1.label) + \
+        edgeBottom.count(game.player1.label)
+
+    edges2 = edgeLeft.count(game.player2.label) + \
+        edgeRight.count(game.player2.label) + \
+        edgeTop.count(game.player2.label) + \
+        edgeBottom.count(game.player2.label)
+
+    if edges1 + edges2 == 0:
+        return 0
+    return 100 * (edges1 - edges2) / (edges1 + edges2)
+
+
+"""
+evalCorners(): calculates the percentage of the pieces in the
+corners of the board that belong to the player that has the turn
+"""
+
+
+def evalCorners(state):
+    game = state.game
+    board = state.board
+    assert isinstance(game, Reversi)  # only Reversi has height and width
+    height = game.height
+    width = game.width
+    corners = [board.get((1, 1)), board.get((width, 1)),
+               board.get((1, height)), board.get((width, height))]
+    corners1 = corners.count(game.player1.label)
+    corners2 = corners.count(game.player2.label)
+    if corners1 + corners2 == 0:
+        return 0
+    return 100 * (corners1 - corners2) / (corners1 + corners2)
---
+++ Practice2/code/Prueba Timeit/demo_reversi.py
@@ -0,0 +1,214 @@
+"""Illustration of a Reversi match.
+
+Authors:
+    Fabiano Baroni <fabiano.baroni@uam.es>,
+    Alejandro Bellogin <alejandro.bellogin@uam.es>
+    Alberto Suárez <alberto.suarez@uam.es>
+
+"""
+
+from __future__ import annotations  # For Python 3.7
+import timeit
+
+from game import Player, TwoPlayerGameState, TwoPlayerMatch
+from heuristic import (
+    heuristic,
+    Heuristic1
+)
+from reversi import (
+    Reversi,
+    from_array_to_dictionary_board,
+    from_dictionary_to_array_board,
+)
+from strategy import (
+    ManualStrategy,
+    MinimaxAlphaBetaStrategy,
+    MinimaxStrategy,
+    RandomStrategy,
+)
+
+player_manual = Player(
+    name='Manual',
+    strategy=ManualStrategy(verbose=0),
+)
+
+player_manual2 = Player(
+    name='Manual_2',
+    strategy=ManualStrategy(verbose=1),
+)
+
+player_random = Player(
+    name='Random',
+    strategy=RandomStrategy(verbose=0),
+    delay=1,
+)
+player_random2 = Player(
+    name='Random_2',
+    strategy=RandomStrategy(verbose=1),
+    delay=2,
+)
+
+player_minimax3 = Player(
+    name='Minimax_3',
+    strategy=MinimaxStrategy(
+        heuristic=heuristic,
+        max_depth_minimax=3,
+        verbose=0,
+    ),
+)
+
+player_minimax4 = Player(
+    name='Minimax_4',
+    strategy=MinimaxStrategy(
+        heuristic=heuristic,
+        max_depth_minimax=4,
+        verbose=0,
+    ),
+)
+
+
+player_alphaBeta = Player(
+    name='AlphaBeta',
+    strategy=MinimaxAlphaBetaStrategy(
+        heuristic=heuristic,
+        max_depth_minimax=3,
+        verbose=0,
+    ),
+)
+
+player_alphaBeta2 = Player(
+    name='AlphaBeta_2',
+    strategy=MinimaxAlphaBetaStrategy(
+        heuristic=heuristic,
+        max_depth_minimax=4,
+        verbose=0,
+    ),
+)
+
+
+"""""""""""""""""""""""""""""""""
+        TIMEIT PLAYERS
+"""""""""""""""""""""""""""""""""
+
+
+timeMinMax1 = Player(
+    name='timeMinMax1',
+    strategy=MinimaxStrategy(
+        heuristic=Heuristic1,
+        max_depth_minimax=3,
+        verbose=0,
+    ),
+)
+
+timeMinMax2 = Player(
+    name='timeMinMax2',
+    strategy=MinimaxStrategy(
+        heuristic=Heuristic1,
+        max_depth_minimax=4,
+        verbose=0,
+    ),
+)
+
+
+timeAlphaBeta1 = Player(
+    name='timeAlphaBeta1',
+    strategy=MinimaxAlphaBetaStrategy(
+        heuristic=Heuristic1,
+        max_depth_minimax=3,
+        verbose=0,
+    ),
+)
+
+timeAlphaBeta2 = Player(
+    name='timeAlphaBeta2',
+    strategy=MinimaxAlphaBetaStrategy(
+        heuristic=Heuristic1,
+        max_depth_minimax=4,
+        verbose=0,
+    ),
+)
+
+# Time players
+# player_a, player_b = timeMinMax1, timeMinMax2
+player_a, player_b = timeAlphaBeta1, timeAlphaBeta2
+
+
+# Manual vs manual player
+# player_a, player_b = player_manual, player_manual2
+
+# Manual vs minimax player
+# player_a, player_b = player_manual, player_minimax4
+
+
+# minimax vs minimax player
+# player_a, player_b = player_minimax3, player_minimax4
+
+# Minimax_AlphaBeta vs Minimax_AlphaBeta player
+# player_a, player_b = player_alphaBeta, player_alphaBeta2
+
+
+"""
+Here you can initialize the player that moves first
+and the board to any valid state.
+E.g., it can be an intermediate state.
+"""
+initial_player = player_a  # Player who moves first.
+
+# Board at an intermediate state of the game.
+initial_board = (
+    ['..B.B..',
+     '.WBBW..',
+     'WBWBB..',
+     '.W.WWW.',
+     '.BBWBWB']
+)
+
+# NOTE Uncoment to use standard initial board.
+# initial_board = None  # Standard initial board.
+
+if initial_board is None:
+    height, width = 8, 8
+else:
+    height = len(initial_board)
+    width = len(initial_board[0])
+    try:
+        initial_board = from_array_to_dictionary_board(initial_board)
+    except ValueError:
+        raise ValueError('Wrong configuration of the board')
+    else:
+        print("Successfully initialised board from array")
+
+
+# Initialize a reversi game.
+game = Reversi(
+    player1=player_a,
+    player2=player_b,
+    height=height,
+    width=width,
+)
+
+# Initialize a game state.
+game_state = TwoPlayerGameState(
+    game=game,
+    board=initial_board,
+    initial_player=initial_player,
+)
+
+# Initialize a match.
+match = TwoPlayerMatch(
+    game_state,
+    max_sec_per_move=1000,
+    gui=False,              # True,
+)
+
+# Play match
+# scores = match.play_match()
+# input('Press any key to finish.')
+
+
+# timeit command
+reps = 10
+tiempo = timeit.timeit("match.play_match()",
+                       setup="from __main__ import match",
+                       number=reps)
+print(tiempo, tiempo/reps)
